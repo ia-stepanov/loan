82f5ca569c1185d4b39da26e532097b86ea4f138
Igor Stepanov <igorstepanov@hotmail.com>
2023-08-12 15:00:52 +0300

    build(Loan Project): создать видеоплеер в модальном окне

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import Slider from './modules/slider';
+import VideoPlayer from './modules/playVideo';
+
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new Slider('.page', '.next');
+  const player = new VideoPlayer('.showup .play', '.overlay');
   
   slider.render();
+  player.init();
 });
